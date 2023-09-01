d9b5beb7de2138ecc22a602974af346927305fbf
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2023-09-01 11:19:01 -0300

    push

---
+++ atividade1Pupo/src/java/greeting/GreetingServlet.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package greeting;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+/**
+ *
+ * @author lmene
+ */
+@WebServlet(name = "GreetingServlet", urlPatterns = {"/greeting.html"})
+public class GreetingServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet GreetingServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>" + new java.util.Date() + "</h3>");
+            // Obtém a hora atual
+            Calendar cal = Calendar.getInstance();
+            int hora = cal.get(Calendar.HOUR_OF_DAY);
+            // Verifica a hora e imprime a saudação correspondente
+            if (hora >= 5 && hora < 12) {
+                out.println("<h3>Bom dia!</h3>");
+            } else if (hora >= 12 && hora < 18) {
+                out.println("<h3>Boa tarde!</h3>");
+            } else if ((hora >= 18 && hora <= 23) || (hora >= 0 && hora < 2)) {
+                out.println("<h3>Boa noite!</h3>");
+            } else {
+                out.println("<h3>Vá dormir!</h3>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
